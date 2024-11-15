182785217dc07cccda51463b06c471ef9df55509
evgeniy466 <123811745+evgeniy466@users.noreply.github.com>
2024-11-15 19:51:59 +0300

    Add files

---
+++ js/app.js
@@ -0,0 +1,56 @@
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+
+ScrollSmoother.create({
+	wrapper: '.wrapper',
+	content: '.content',
+    effects: true
+})
+
+gsap.fromTo('.hero', { opacity: 1 }, { 
+    opacity: 0,
+    scrollTrigger: {
+        trigger: '.hero',
+        start: '.center',
+        end: 'bottom',
+        scrub: true
+    }
+})
+
+ let priceItemsLeft = gsap.utils.toArray('.pricelist__left .pricelist__item');
+
+ priceItemsLeft.forEach(item => {
+    gsap.fromTo(item, {x: -100 ,opacity: 0}, {
+        opacity: 1, x: 0,
+        scrollTrigger: {
+            trigger: item,
+            start: '-850',
+            end: '-220',
+            scrub: true
+        }
+    })
+ })
+
+ let priceItemsRight = gsap.utils.toArray('.pricelist__right .pricelist__item');
+
+ priceItemsRight.forEach(item => {
+    gsap.fromTo(item, {x: 100 ,opacity: 0}, {
+        opacity: 1, x: 0,
+        scrollTrigger: {
+            trigger: item,
+            start: '-850',
+            end: '-230',
+            scrub: true
+        }
+    })
+ })
+
+let heroBlock = document.querySelector(".hero");
+heroBlock.addEventListener('mousemove', e => {
+    Object.assign(document.documentElement, {
+        style: `
+        --move-x: ${(e.clientX - window.innerWidth / 2) * -.005}deg;
+        --move-y: ${(e.clientY - window.innerHeight / 2) * -.01}deg;
+        `
+    })
+})
+
