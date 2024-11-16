c1362c06b86af620ecef3fad728ecceb300a69a9
evgeniy466 <bondar.evgeniy3@gmail.com>
2024-11-16 21:43:14 +0300

    Added new styles and layout parts

--- js/app.js
+++ js/app.js
@@ -54,3 +54,13 @@ heroBlock.addEventListener('mousemove', e => {
     })
 })
 
+const swiper = new Swiper('.swiper', {
+    direction: "vertical",
+    slidesPerView: 1,
+    spaceBetween: 30,
+    mousewheel: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
